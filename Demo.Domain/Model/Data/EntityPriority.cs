d2df46ee48094c7eb8fdd44eac572e0dc612f3f9
Sara <Sh6197174@gmail.com>
2024-07-15 19:55:44 +0300

    serilize objects

--- Demo.Domain/Model/Data/EntityPriority.cs
+++ Demo.Domain/Model/Data/EntityPriority.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +12,28 @@ namespace Demo.Domain.Model.Data
     {
         public string EntityType { get; set; }
         public Dictionary<string, object> Priorities { get; set; }
+
+        [JsonConstructor]
+        public EntityPriority(string entityType, Dictionary<string, JToken> priorities)
+        {
+            EntityType = entityType;
+            Priorities = priorities.ToDictionary(
+                kvp => kvp.Key,
+                kvp => ConvertJToken(kvp.Value)
+            );
+        }
+
+        private static object ConvertJToken(JToken token)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Array:
+                    return token.ToObject<List<string>>() ?? new List<string>();
+                case JTokenType.Object:
+                    return token.ToObject<Dictionary<string, List<string>>>() ?? new Dictionary<string, List<string>>();
+                default:
+                    throw new InvalidOperationException("Unexpected token type: " + token.Type);
+            }
+        }
     }
 }
